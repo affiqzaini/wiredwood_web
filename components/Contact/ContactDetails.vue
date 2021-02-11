48ef710a347d90dcdbf9429c77e30e83d2df5ec9
affiqzaini <off.affiqzaini@gmail.com>
2021-02-11 16:46:46 +0800

    feat: remove phone number

--- components/Contact/ContactDetails.vue
+++ components/Contact/ContactDetails.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="components__contactDetails center-all">
     <div class="mx-auto">
-      <div class="text-left">
+      <div class="text-left mb-8">
         <p class="mb-0">name : affiq zaini</p>
         <p class="mb-0">email : off.affiqzaini@gmail.com</p>
-        <p>phone : +601 - 11 1128 3226</p>
       </div>
 
       <div class="text-left">
         <p class="mb-0">name : eirsyad qawiem</p>
         <p class="mb-0">email : eirsyad.projects@gmail.com</p>
-        <p>phone : +601 - 12 211 0991</p>
       </div>
     </div>
   </div>
